6a1dbe9b3561d73ae671ea35f9d9dc1ef5ad67f6
Ilya Yudin <nibezo.cs@gmail.com>
2022-11-02 14:13:05 +0300

    feat: add error check and regex

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 function calculator(string) {
-    if (string[0] > 0) {  
+    // Регулярные выражения для проверки выражений 
+    const arabic = /^([1-9]|[1-9][0]).[+*/-].([1-9]|[1-9][0])$/
+    const roman = /^([XVI]|[VI/VII/VIII/IIX/IX]).[+*/-].([XVI]|[VI/VII/VIII/IIX/IX])$/
+    
+    // Вычисляем десятичное выражение
+    if (arabic.test(string)) {
+      // Получаем операнды
       firstOperand = parseInt(string.slice(0, string.indexOf(' ')))
       secondOperand = parseInt(string.slice(string.indexOf(' ') + 3))
-      if (string[string.indexOf(' ') + 1] === '+')  {
-        return String(firstOperand + secondOperand)
-      } else if (string[string.indexOf(' ') + 1] === '-') {
-        return String(firstOperand - secondOperand)
-      } else if (string[string.indexOf(' ') + 1] === '*') {
-        return String(firstOperand * secondOperand)
-      } else if (string[string.indexOf(' ') + 1] === '/') {
-        return String(parseInt(firstOperand / secondOperand))
-      }
-      } else {
-      console.log('Это не десятичное!')
+      if (firstOperand )
+      // В зависимости от оператора возвращаем значение десятичного выражения
+      if (string[string.indexOf(' ') + 1] === '+')  { return String(firstOperand + secondOperand)} 
+      else if (string[string.indexOf(' ') + 1] === '-') { return String(firstOperand - secondOperand)} 
+      else if (string[string.indexOf(' ') + 1] === '*') { return String(firstOperand * secondOperand)} 
+      else if (string[string.indexOf(' ') + 1] === '/') { return String(parseInt(firstOperand / secondOperand))}
+      } else if (roman.test(string)) {
+      
+    } else {
+      throw new UserException("Некорректный ввод данных");    
     }
   }
+  
   module.exports = calculator; // Не трогайте эту строчку
